74256dbbac182bcdaa1974e60ffcd464e1e9a77f
ryo-watanabe <ryobotsound@docomo.ne.jp>
2021-06-22 15:47:07 +0900

    Controller test fixed and lint again

--- controller_test.go
+++ controller_test.go
@@ -481,7 +481,7 @@ func checkAction(expected, actual core.Action, t *testing.T) {
 func filterInformerActions(actions []core.Action) []core.Action {
 	ret := []core.Action{}
 	for _, action := range actions {
-		if action.GetNamespace() == snapshotNamespace && (action.Matches("get", "objectstoreconfigs") ||
+		if (action.Matches("get", "objectstoreconfigs") ||
 			action.Matches("list", "volumesnapshots") ||
 			action.Matches("get", "volumesnapshots") ||
 			action.Matches("watch", "volumesnapshots")) {
